d8ff5d6817c6a167decc6be0b4112a7727130b0d
Mana Misawa <mnmisawa04@gmail.com>
2023-08-10 18:14:38 +0900

    Done the challenge

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :lists, only: %i[index show new create] do
-    resources :bookmarks, only: %i[new create destroy]
+    resources :bookmarks, only: %i[new create]
   end
+  resources :bookmarks, only: :destroy
 end
