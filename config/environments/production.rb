8e502307fec44563ed7e87175af4c02515da98c9
Mana Misawa <mnmisawa04@gmail.com>
2023-08-11 12:38:08 +0900

    Added cloudinary

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # cloudinary
+  config.active_storage.service = :cloudinary
 end
